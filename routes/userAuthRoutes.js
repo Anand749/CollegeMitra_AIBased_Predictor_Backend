5bc2b35c2a1e7a5ebc3a25d33a2314ecb4dbfb0d
Anand Chapke <anandchapke11@gmail.com>
2026-07-20 21:15:37 +0530

    COmment Change

--- routes/userAuthRoutes.js
+++ routes/userAuthRoutes.js
@@ -7,7 +7,7 @@ const { sendOtp, registerUser, loginUser, sendResetOtp, resetPassword } = requir
 const otpLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 3, // Limit each IP to 3 requests per `window`
-    message: { success: false, message: 'Too many OTP requests from this IP, please try again after 15 minutes' },
+    message: { success: false, message: 'Too many OTP requests from this IP' },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
